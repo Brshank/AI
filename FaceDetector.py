5bb1a5f87cabc8ee2f0f8af223b9c3172d7f882b
Brshank Singh Negi <brsshanksn@gmail.com>
2021-06-07 22:35:22 +0530

    init

--- FaceDetector.py
+++ FaceDetector.py
@@ -1,6 +1,6 @@
 import cv2
 trained_facedata=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 img=cv2.imread('modiji.jpg')
-cv2.imshow('Brshank',img)
+cv2.imshow('Negi Face Detector',img)
 cv2.waitKey()
 print('wow it works')
