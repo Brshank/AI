2bc8c70e16d28f821ee5c455fb6f13d344b72e25
Brshank Singh Negi <brsshanksn@gmail.com>
2021-06-08 00:17:43 +0530

    init

--- FaceDetector.py
+++ FaceDetector.py
@@ -1,19 +1,21 @@
 import numpy as np
 import array as arr 
 import cv2
+from numpy.core.fromnumeric import size
 
 trained_face_data=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-img=cv2.imread('modiji.jpg')
+img=cv2.imread('grp.jpg')
 
 #tranform color to grayscale
 grayscaled_img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 # Detect faces
 face_coordinates=trained_face_data.detectMultiScale(grayscaled_img)
+# print(len(face_coordinates))
 
-(x,y,w,h)=face_coordinates[0]
-
+(x,y,w,h)=face_coordinates[i]
 cv2.rectangle(img, (x,y) , (x+w,y+h),  (0,255,255) , 3)
+
 
 cv2.imshow('Greyscaled detector',img)
 cv2.waitKey()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,7 +1,5 @@
 
-cv2.rectangle(img,(x,y),((x+w),(y+h)),(0,0,255),2)
-
-cv2.imshow('Greyscaled detector',grayscaled_img)
-cv2.waitKey()
-
-print('wow it works')
+# while i <= len(face_coordinates):
+#     (x,y,w,h)=face_coordinates[i]
+#     cv2.rectangle(img, (x,y) , (x+w,y+h),  (0,255,255) , 3)
+#     i+=1
