419a13c8afba21747077230db8741d6325bc5d50
Brshank Singh Negi <brsshanksn@gmail.com>
2021-06-24 20:44:51 +0530

    init

--- CLassifier.py
+++ CLassifier.py
@@ -1,15 +1,12 @@
-ctr=0
-j=0
-i=0
-x=0
+ctr=0,j=0,i=0,x=0
 with open("bin.txt", "r") as f:
     findrun=f.read()
 run=int(input("how many runs for 1's do u want="))
 print(run)
-x=len(findrun)-run
+x=int(len(findrun)-run)
 while i < x:
     while j<(run-1):
         if findrun[j]==1:
             ctr+=1
     j+=1
-    
+print('number of counters of run for=',ctr)
--- findrun.py
+++ findrun.py
@@ -1,24 +1,14 @@
-# with open("bin.txt", "r") as f:
-#     findrun=f.read()
-# print(findrun)
-# ctr=0
-# i=0
-# int(i)
-# for i in findrun:
-#     if "1"==findrun[i]:
-#         if "1"==findrun[i+1]:
-#             if "1"==findrun[i+2]:
-#                 ctr+=1
-#     else:continue
-#     i+=1
-# print(ctr)
+with open("bin.txt", "r") as f:
+    findrun=f.read()
+print(findrun)
 ctr=0
-j=0
-run=input("how many runs for 1's do u want=")
-print(run)
-for i=(len(findrun)-run):
-    while j<(run-1):
-        if findrun[j]==1:
-        ctr+=1
-    j+=1
-    
+i=0
+int(i)
+for i in findrun:
+    if "1"==findrun[i]:
+        if "1"==findrun[i+1]:
+            if "1"==findrun[i+2]:
+                ctr+=1
+    else:continue
+    i+=1
+print(ctr)
