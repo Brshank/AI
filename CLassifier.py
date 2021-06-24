ed8f0e09f3a693a97cd9e2883b0034d1e088c765
Brshank Singh Negi <brsshanksn@gmail.com>
2021-06-24 22:57:42 +0530

    init

--- CLassifier.py
+++ CLassifier.py
@@ -2,6 +2,8 @@ ctr=0
 j=0
 i=0
 x=0
+z=0
+y=0
 with open("bin.txt", "r") as f:
     findrun=f.read()
 run=int(input("how many runs for 1's do u want="))
@@ -10,4 +12,6 @@ for z in range(i,x):
     for  y in range(j,(run-1)):
         if findrun[j]=='1':
             ctr+=1
+            
+
 print('number of counters of run for=',ctr)
