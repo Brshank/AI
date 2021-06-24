2c7464111746b5e9b7eeea3f468a54fcab35f39b
Brshank Singh Negi <brsshanksn@gmail.com>
2021-06-24 20:40:16 +0530

    init

--- findrun.py
+++ findrun.py
@@ -1,14 +1,24 @@
-with open("bin.txt", "r") as f:
-    findrun=f.read()
-print(findrun)
+# with open("bin.txt", "r") as f:
+#     findrun=f.read()
+# print(findrun)
+# ctr=0
+# i=0
+# int(i)
+# for i in findrun:
+#     if "1"==findrun[i]:
+#         if "1"==findrun[i+1]:
+#             if "1"==findrun[i+2]:
+#                 ctr+=1
+#     else:continue
+#     i+=1
+# print(ctr)
 ctr=0
-i=0
-int(i)
-for i in findrun:
-    if "1"==findrun[i]:
-        if "1"==findrun[i+1]:
-            if "1"==findrun[i+2]:
-                ctr+=1
-    else:continue
-    i+=1
-print(ctr)
+j=0
+run=input("how many runs for 1's do u want=")
+print(run)
+for i=(len(findrun)-run):
+    while j<(run-1):
+        if findrun[j]==1:
+        ctr+=1
+    j+=1
+    
