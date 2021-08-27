e3df3c848f0a08367c12caf81ab8741ffc4c20e4
Brshank Singh Negi <brsshanksn@gmail.com>
2021-08-27 09:28:46 +0530

    init

--- Uni/OS_SJF.cpp
+++ Uni/OS_SJF.cpp
@@ -1,89 +1,117 @@
 #include <iostream>
-#include<algorithm>
+#include <algorithm> 
+#include <iomanip>
+#include <string.h> 
 using namespace std;
-void SJF()
-{
-    float average_TAT=0.0, average_WT=0.0;
-    int number=0,CompileTime=0,ArrivalTime=0,BurstTime=0,TurnAroundTime=0,WaitingTime=0;
-    int sum1=0,sum2=0;
-    cout<<"SJF (Shortest Job First), Non Preemptive:\n";
-    cout<<"Enter the Number of processes: ";
-    cin>>number; 
-    int arr[number][6]={0}; 
-    //For inputting the values in the Gantt Chart!
-    for(int i=0;i<number;i++)
-    {
-        cout<<"Process "<<i+1<<":"<<endl;
-        cout<<"Arrival Time : ";
-        cin>>ArrivalTime;
-        cout<<"Burst Time : ";
-        cin>>BurstTime;
-        arr[i][0]=i+1;
-        arr[i][1]=ArrivalTime;
-        arr[i][2]=BurstTime;
+
+struct process {
+    int pid;
+    int arrival_time;
+    int burst_time;
+    int start_time;
+    int completion_time;
+    int turnaround_time;
+    int waiting_time;
+    int response_time;
+};
+
+int main() {
+
+    int n;
+    struct process p[100];
+    float avg_turnaround_time;
+    float avg_waiting_time;
+    float avg_response_time;
+    float cpu_utilisation;
+    int total_turnaround_time = 0;
+    int total_waiting_time = 0;
+    int total_response_time = 0;
+    int total_idle_time = 0;
+    float throughput;
+    int is_completed[100];
+    memset(is_completed,0,sizeof(is_completed));
+
+    cout << setprecision(2) << fixed;
+
+    cout<<"Enter the number of processes: ";
+    cin>>n;
+    for(int i = 0; i < n; i++) {
+        cout<<"Enter arrival time of process "<<i+1<<": ";
+        cin>>p[i].arrival_time;
+        cout<<"Enter burst time of process "<<i+1<<": ";
+        cin>>p[i].burst_time;
+        p[i].pid = i+1;
+        cout<<endl;
     }
-    
-    for(int i=0;i<number;i++)
-    {
-        if(i==0)    //if only one process CT=BT
-        {
-            arr[i][3]=arr[i][2];
-        }
-        else
-        {
-            if(arr[i][1]<=arr[i-1][3])  //AT<=CT of prev process
-            {
-                arr[i][3]=arr[i-1][3]+arr[i][2];
+
+    int current_time = 0;
+    int completed = 0;
+    int prev = 0;
+
+    while(completed != n) {
+        int idx = -1;
+        int mn = 10000000;
+        for(int i = 0; i < n; i++) {
+            if(p[i].arrival_time <= current_time && is_completed[i] == 0) {
+                if(p[i].burst_time < mn) {
+                    mn = p[i].burst_time;
+                    idx = i;
+                }
+                if(p[i].burst_time == mn) {
+                    if(p[i].arrival_time < p[idx].arrival_time) {
+                        mn = p[i].burst_time;
+                        idx = i;
+                    }
+                }
             }
-            else
-            {
-                int blank=arr[i][1]-arr[i-1][3];
-                arr[i][3]=arr[i-1][3]+arr[i][2]+blank;
-            }
         }
-    }
-    
-    for(int i=0;i<number;i++)
-    {
-        // TAT = CT - AT
-        arr[i][4] = arr[i][3] - arr[i][1];
+        if(idx != -1) {
+            p[idx].start_time = current_time;
+            p[idx].completion_time = p[idx].start_time + p[idx].burst_time;
+            p[idx].turnaround_time = p[idx].completion_time - p[idx].arrival_time;
+            p[idx].waiting_time = p[idx].turnaround_time - p[idx].burst_time;
+            p[idx].response_time = p[idx].start_time - p[idx].arrival_time;
+            
+            total_turnaround_time += p[idx].turnaround_time;
+            total_waiting_time += p[idx].waiting_time;
+            total_response_time += p[idx].response_time;
+            total_idle_time += p[idx].start_time - prev;
+
+            is_completed[idx] = 1;
+            completed++;
+            current_time = p[idx].completion_time;
+            prev = current_time;
+        }
+        else {
+            current_time++;
+        }
         
-        // WT = TAT - BT
-        arr[i][5] = arr[i][4] - arr[i][2];
-        
-        //Sum of the Turn Around Time
-        sum1+=arr[i][4];
-        //Sum of the Waiting Time
-        sum2+=arr[i][5];
     }
-    
-    //Full Forms
-    cout<<endl;
-    cout<<"Arrival Time -> AT"<<endl;
-    cout<<"Burst Time   -> BT"<<endl;
-    cout<<"Compile Time -> CT"<<endl;
-    cout<<"Turn Around Time -> TAT"<<endl;
-    cout<<"Waiting Time -> WT"<<endl;
-    cout<<endl;
-    
-    // Display the Table
-    cout<<"|No"<<"   |   "<<"AT"<<"    |    "<<"BT"<<"    |    "<<"CT"<<"    |    "<<"TAT"<<"    |    "<<"WT"<<"    |    "<<"RT"<<endl;
-    for(int i=0;i<number;i++)
-    {
-        cout<<"_____________________________________________________________"<<endl;
-        cout<<"|"<<arr[i][0]<<"    |    "<<arr[i][1]<<"    |    "<<arr[i][2]<<"    |    "<<arr[i][3]<<"    |      "<<arr[i][4]<<"    |    "<<arr[i][5]<<"    |    "<<arr[i][5]<<endl;
+
+    int min_arrival_time = 10000000;
+    int max_completion_time = -1;
+    for(int i = 0; i < n; i++) {
+        min_arrival_time = min(min_arrival_time,p[i].arrival_time);
+        max_completion_time = max(max_completion_time,p[i].completion_time);
     }
-    
-    cout<<endl;
-    average_TAT=(float)sum1/(float)number;
-    average_WT=(float)sum2/(float)number;
-    cout<<"Average Turn Around Time: "<<average_TAT<<endl;
-    cout<<"Average Waiting Time: "    <<average_WT<<endl;
-   
-    
+
+    avg_turnaround_time = (float) total_turnaround_time / n;
+    avg_waiting_time = (float) total_waiting_time / n;
+    avg_response_time = (float) total_response_time / n;
+    cpu_utilisation = ((max_completion_time - total_idle_time) / (float) max_completion_time )*100;
+    throughput = float(n) / (max_completion_time - min_arrival_time);
+
+    cout<<endl<<endl;
+
+    cout<<"#P\t"<<"AT\t"<<"BT\t"<<"ST\t"<<"CT\t"<<"TAT\t"<<"WT\t"<<"RT\t"<<"\n"<<endl;
+
+    for(int i = 0; i < n; i++) {
+        cout<<p[i].pid<<"\t"<<p[i].arrival_time<<"\t"<<p[i].burst_time<<"\t"<<p[i].start_time<<"\t"<<p[i].completion_time<<"\t"<<p[i].turnaround_time<<"\t"<<p[i].waiting_time<<"\t"<<p[i].response_time<<"\t"<<"\n"<<endl;
+    }
+    cout<<"Average Turnaround Time = "<<avg_turnaround_time<<endl;
+    cout<<"Average Waiting Time = "<<avg_waiting_time<<endl;
+    cout<<"Average Response Time = "<<avg_response_time<<endl;
+    cout<<"CPU Utilization = "<<cpu_utilisation<<"%"<<endl;
+    cout<<"Throughput = "<<throughput<<" process/unit time"<<endl;
+
 }
-int main()
-{
-    SJF();
-    return 0;
-} 
--- Uni/linkedlist.cpp
+++ Uni/linkedlist.cpp
@@ -1,31 +1,179 @@
 #include<iostream>
 using namespace std;
-class Node{
-
-    int data,next;
-    node*next;
-    public:
-    void Insert(int value);
-    void Display();
+struct node
+{
+	int data;
+	node *next;	
+};
+class list
+{
+		private:
+		node *head, *tail;
+		public:
+		list()
+		{
+			head=NULL;
+			tail=NULL;
+		}
+		void createnode(int value)
+		{
+			node *temp=new node;
+			temp->data=value;
+			temp->next=NULL;
+			if(head==NULL)
+			{
+				head=temp;
+				tail=temp;
+				temp=NULL;
+			}
+			else
+			{	
+				tail->next=temp;
+				tail=temp;
+			}
+		}
+		void display()
+		{
+            cout<<"\n--------------------------------------------------\n";
+            cout<<"---------------Displaying All nodes---------------";
+            cout<<"\n--------------------------------------------------\n";
+			node *temp=new node;
+			temp=head;
+			while(temp!=NULL)
+			{
+				cout<<temp->data<<"\t";
+				temp=temp->next;
+			}
+		}
+		void insert_start(int value)
+		{
+            cout<<"\n--------------------------------------------------\n";
+            cout<<"----------------Inserting At Start----------------";
+            cout<<"\n--------------------------------------------------\n";
+			node *temp=new node;
+			temp->data=value;
+			temp->next=head;
+			head=temp;
+		}
+		void insert_position(int pos, int value)
+		{
+            cout<<"\n--------------------------------------------------\n";
+            cout<<"-------------Inserting At Particular--------------";
+            cout<<"\n--------------------------------------------------\n";
+			node *pre=new node;
+			node *cur=new node;
+			node *temp=new node;
+			cur=head;
+			for(int i=1;i<pos;i++)
+			{
+				pre=cur;
+				cur=cur->next;
+			}
+			temp->data=value;
+			pre->next=temp;	
+			temp->next=cur;
+		}
+		void delete_first()
+		{
+            cout<<"\n--------------------------------------------------\n";
+            cout<<"----------------Deleting At Start-----------------";
+            cout<<"\n--------------------------------------------------\n";
+			node *temp=new node;
+			temp=head;
+			head=head->next;
+			delete temp;
+		}
+		void delete_last()
+		{
+            cout<<"\n--------------------------------------------------\n";
+            cout<<"-----------------Deleting At End-------------------";
+            cout<<"\n--------------------------------------------------\n";
+			node *current=new node;
+			node *previous=new node;
+			current=head;
+			while(current->next!=NULL)
+			{
+				previous=current;
+				current=current->next;	
+			}
+			tail=previous;
+			previous->next=NULL;
+			delete current;
+		}
+		void delete_position(int pos)
+		{
+            cout<<"\n--------------------------------------------------\n";
+            cout<<"--------------Deleting At Particular--------------";
+            cout<<"\n--------------------------------------------------\n";
+			node *current=new node;
+			node *previous=new node;
+			current=head;
+			for(int i=1;i<pos;i++)
+			{
+				previous=current;
+				current=current->next;
+			}
+			previous->next=current->next;
+		}
+};
+int main()
+{
+	list obj;
+	while(choice!=9)
+{   
+   cout << "Menu\n1.Insert Value\n2.Insert at Particular Index\n3.Delete at Particular Index\n4.Print\n5.Insert at Start\n6.Insert at End \n7.Delete at Start \n8. Delete at End\n9.Exit\nChoice: ";
+   cin>>choice;
+	if(choice==1)
+	{
+		int number;
+		cout << "Enter Number: ";
+		cin >> number;
+		obj.createnode(number);
+	}
+	else if(choice==2)
+	{
+		int index, number;
+		cout << "Enter Index at which value will take place:";
+		cin>>index;
+		cout << "Enter Number to be inserted: ";
+		cin>>number;
+		obj.insert_position(index,number);
+	}
+	else if(choice==3)
+	{
+		int index;
+		cout << "Enter Index: ";
+		cin>>index;
+		obj.delete_position(index);
+	}
+	else if(choice==4)
+	{   cout << obj.display();
+	}
+	else if(choice==5)
+	{   int number;
+		cout << "Enter Number: ";
+		cin >> number;
+        cout << obj.insert_start(number);
+	}
+	else if(choice==6)
+	{   int number;
+        cout<<"\n--------------------------------------------------\n";
+        cout<<"-----------------Inserting At End-----------------";
+        cout<<"\n--------------------------------------------------\n";
+	{   cout << "Enter Number: ";
+		cin >> number;
+        cout << obj.createnode(number);
+	}
+	else if(choice==7)
+	{   cout << obj.delete_first();
+	}
+	else if(choice==8)
+	{   cout << obj.delete_last();
+	}
+	
 }
-Node::Insert(int value){
-    node*end=newnode;
-    
-    end->next=;
-    end->data=value;
-    end->next=NULL;
+cout<<"Program Has Ended Successfully";
+exit(0);
+return 0;
 
-}
-
-Node::Display(){
-    node* currentNode = head;
-    while (currentNode!=NULL){
-        cout<<currentNode->data<<endl;
-        currentNode=currentNode->next;
-    }
-}
-
-int main(){
-    
-    return 0;
 }
