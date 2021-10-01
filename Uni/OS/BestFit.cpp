6a6312dc43c599db4051ab596de8afb8b8a86730
Brshank Singh Negi <brsshanksn@gmail.com>
2021-10-01 23:35:11 +0530

    init

--- Uni/OS/BestFit.cpp
+++ Uni/OS/BestFit.cpp
@@ -4,64 +4,58 @@
 using namespace std;
 
 
-void bestFit(int blockSize[], int m, int processSize[], int n)
+void bestFit(int BlockMemorySize[], int m, int ProcessMemorySize[], int n)
 {
-    // Stores block id of the block allocated to a
-    // process
-    int allocation[n];
+
+    int CheckAllocation[n];
   
-    // Initially no block is assigned to any process
-    memset(allocation, -1, sizeof(allocation));
+    
+    memset(CheckAllocation, -1, sizeof(CheckAllocation));
   
-    // pick each process and find suitable blocks
-    // according to its size ad assign to it
     for (int i=0; i<n; i++)
     {
-        // Find the best fit block for current process
-        int bestIdx = -1;
+       
+        int BestFitChecker = -1;
         for (int j=0; j<m; j++)
         {
-            if (blockSize[j] >= processSize[i])
+            if (BlockMemorySize[j] >= ProcessMemorySize[i])
             {
-                if (bestIdx == -1)
-                    bestIdx = j;
-                else if (blockSize[bestIdx] > blockSize[j])
-                    bestIdx = j;
+                if (BestFitChecker == -1)
+                    BestFitChecker = j;
+                else if (BlockMemorySize[BestFitChecker] > BlockMemorySize[j])
+                    BestFitChecker = j;
             }
         }
   
-        // If we could find a block for current process
-        if (bestIdx != -1)
+       
+        if (BestFitChecker != -1)
         {
-            // allocate block j to p[i] process
-            allocation[i] = bestIdx;
-  
-            // Reduce available memory in this block.
-            blockSize[bestIdx] -= processSize[i];
+           
+            CheckAllocation[i] = BestFitChecker;
+            BlockMemorySize[BestFitChecker] -= ProcessMemorySize[i];
         }
     }
   
     cout << "\nProcess No.\tProcess Size\tBlock no.\n";
     for (int i = 0; i < n; i++)
     {
-        cout << "   " << i+1 << "\t\t" << processSize[i] << "\t\t\n";
-        if (allocation[i] != -1)
-            cout << allocation[i] + 1;
+        cout << "   " << i+1 << "\t\t" << ProcessMemorySize[i] << "\t\t\n";
+        if (CheckAllocation[i] != -1)
+            cout << CheckAllocation[i] + 1;
         else
             cout << "Not Allocated";
         cout << endl;
     }
 }
   
-// Driver code
 int main()
 {
-    int blockSize[] = {100, 500, 200, 300, 600};
-    int processSize[] = {212, 417, 112, 426};
-    int m = sizeof(blockSize)/sizeof(blockSize[0]);
-    int n = sizeof(processSize)/sizeof(processSize[0]);
+    int BlockMemorySize[] = {100, 500, 200, 300, 600};
+    int ProcessMemorySize[] = {212, 417, 112, 426};
+    int m = sizeof(BlockMemorySize)/sizeof(BlockMemorySize[0]);
+    int n = sizeof(ProcessMemorySize)/sizeof(ProcessMemorySize[0]);
   
-    bestFit(blockSize, m, processSize, n);
+    bestFit(BlockMemorySize, m, ProcessMemorySize, n);
   
     return 0 ;
 }
