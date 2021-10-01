e2bed08bf84902a7ea6f18f90c81c11bfc3c912d
Brshank Singh Negi <brsshanksn@gmail.com>
2021-10-01 23:29:06 +0530

    init

--- Uni/OS/BestFit.cpp
+++ Uni/OS/BestFit.cpp
@@ -1,41 +1,67 @@
 #include<iostream>
-#include <cstring>
+#include<stdio.h>
+#include<cstring>
 using namespace std;
-class BestFit
-{
-public:
-void logic(int Memorysize[],int a,int ProcessSize[],int b)
-{
-int CheckAllocation[b];
-memset(CheckAllocation,-1,sizeof(CheckAllocation));
 
-}
-void display(int Memorysize[],int a,int ProcessSize[],int b)
+
+void bestFit(int blockSize[], int m, int processSize[], int n)
 {
+    // Stores block id of the block allocated to a
+    // process
+    int allocation[n];
+  
+    // Initially no block is assigned to any process
+    memset(allocation, -1, sizeof(allocation));
+  
+    // pick each process and find suitable blocks
+    // according to its size ad assign to it
+    for (int i=0; i<n; i++)
+    {
+        // Find the best fit block for current process
+        int bestIdx = -1;
+        for (int j=0; j<m; j++)
+        {
+            if (blockSize[j] >= processSize[i])
+            {
+                if (bestIdx == -1)
+                    bestIdx = j;
+                else if (blockSize[bestIdx] > blockSize[j])
+                    bestIdx = j;
+            }
+        }
+  
+        // If we could find a block for current process
+        if (bestIdx != -1)
+        {
+            // allocate block j to p[i] process
+            allocation[i] = bestIdx;
+  
+            // Reduce available memory in this block.
+            blockSize[bestIdx] -= processSize[i];
+        }
+    }
+  
     cout << "\nProcess No.\tProcess Size\tBlock no.\n";
-	for (int i = 0; i < n; i++)
-	{
-		cout << " " << i+1 << "\t\t" << ProcessSize[i] << "\t\t";
-		if (CheckAllocation[i] != -1)
-			cout << CheckAllocation[i] + 1;
-		else
-			cout << "Not Allocated";
-		cout << endl;
-	}
+    for (int i = 0; i < n; i++)
+    {
+        cout << "   " << i+1 << "\t\t" << processSize[i] << "\t\t\n";
+        if (allocation[i] != -1)
+            cout << allocation[i] + 1;
+        else
+            cout << "Not Allocated";
+        cout << endl;
+    }
 }
-
-};
-
-
-int main(){
-// 212,417,112,426 processes to be implemented using Best fiot
-int Memorysize[]={100,500,200,300,600};
-int ProcessSize[]={417,212,426,112};
-int a = sizeof(Memorysize)/sizeof(Memorysize[0]);
-int b = sizeof(ProcessSize)/sizeof(ProcessSize[0]);  
-    
-  BestFit BF;
-  BF.logic(Memorysize[],a,ProcessSize[],b);
-
-    return 0;
+  
+// Driver code
+int main()
+{
+    int blockSize[] = {100, 500, 200, 300, 600};
+    int processSize[] = {212, 417, 112, 426};
+    int m = sizeof(blockSize)/sizeof(blockSize[0]);
+    int n = sizeof(processSize)/sizeof(processSize[0]);
+  
+    bestFit(blockSize, m, processSize, n);
+  
+    return 0 ;
 }
