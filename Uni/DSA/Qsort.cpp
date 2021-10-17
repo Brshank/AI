483790a5f7c8c6bad65c82398a2cdde56a4c5c5b
Brshank Singh Negi <brsshanksn@gmail.com>
2021-10-17 20:13:47 +0530

    init

--- Uni/DSA/Qsort.cpp
+++ Uni/DSA/Qsort.cpp
@@ -3,73 +3,62 @@ using namespace std;
 class Qsort
 {
 public:
-void swap(int *a,int *b)
-{
-    int t=*a;
-    *a=*b;
-    *b=t;
-}
-int partition(int array[],int low,int high)
-{
-    int i=low;
-    int j=high;
-    int pivot=low;
-    while(i<j)
-    {
-        do{
-            i++;
-        }while(array[i]<=array[pivot]);
-        while (array[j] >array[pivot])
-        {
-            j--;
-        }
-        if(i<j)
-        {
-            swap(&array[i],&array[j]);
-        }
-
-    }
-    swap(&array[low],&array[j]);
-    return j;
-}
-void quicksort(int array[], int low,int high )
-{
-    if(low<high)
-    {
-        int j=partition(array,low,high);
-        quicksort(array,low,j);
-        quicksort(array,j+1,high);
-    }
-}
-
-};
-
-int main()
-{ 
-     Qsort q;
-    int arr[]={5, 1, 0, 5, 26, 47, 10,99, 23,32};
-    q.quicksort(arr,0,9);
-    for(int i=0;i<10;i++)
-    {
-        cout<<arr[i]<<",";
-    }
-    cout<<endl;
-    return 0;
-}
-
 void print(int arr[], int n)
 {
-    cout <<"{";
-    for (int i = 0; i < n; i++)
-    {
-        if (i + 1 != n)
-            {
-            cout << arr[i] ;
-            cout<<",";
-            }
-        else
-            cout << arr[i];
-    }
-    cout << "}";
+	cout << "{";
+	for (int i = 0; i < n; i++) {
+		if (i + 1 != n)
+			cout << arr[i] << ",";
+		else
+			cout << arr[i];
+	}
+	cout << "}";
+}
+void swap(int& m, int& n) {
+	int temp = m;
+	m = n;
+	n = temp;
+}
+int RandomizedPartition(int* arr, int p, int q) {
+	int r = p + rand() % (q - p + 1);
+	swap(arr[r], arr[p]);
+	int pivot = arr[p];
+	int i = p;
+	for (int j = p + 1; j <= q; j++) {
+		if (arr[j] <= pivot) {
+			i = i + 1;
+			swap(arr[i], arr[j]);
+		}
+	}
+	swap(arr[p], arr[i]);
+	return i;
+}
 
+void RandomizedQuicksort(int* arr, int p, int q) {
+	if (p < q) {
+		int r = RandomizedPartition(arr, p, q);
+		RandomizedQuicksort(arr, p, r - 1);
+		RandomizedQuicksort(arr, r + 1, q);
+	}
+}
+};
+
+int main() {
+
+    Qsort q;
+	//Generate input array
+    cout<<endl;
+    int arr[]={-8,5,1,0,5,26,47,10,99,23,32};
+    //Print input array
+    cout << "Input array: ";
+    q.print(arr, 11);
+    
+    q.RandomizedQuicksort(arr, 0, 10);
+    
+    //Print output array
+    cout << "\nOutput array: ";
+    q.print(arr, 11);
+    cout << endl;
+
+	return 0;
 }
